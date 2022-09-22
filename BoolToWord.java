db02562ce771c96f5057ebd4f4def7281cc1d275
Bodiseowei Martin Tonweriyai <bmtonweriyai@gmail.com>
2022-09-22 10:16:57 +0100

    booToWord solution

---
+++ BoolToWord.java
@@ -0,0 +1,10 @@
+public class BoolToWord {
+//    Convert boolean values to strings 'Yes' or 'No'.
+//
+//    Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+    public static String boolToWord(boolean b)
+    {
+        //TODO
+        return (b) ? "Yes" : "No";
+    }
+}
