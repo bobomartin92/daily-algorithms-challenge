d551c54aa13489bcda327429ad7ca0ee41a2ab29
Bodiseowei Martin Tonweriyai <bmtonweriyai@gmail.com>
2022-09-30 17:23:38 +0100

    added Calculate average solution

---
+++ dailyAlgorithms/CalculateAverage.java
@@ -0,0 +1,23 @@
+package dailyAlgorithms;
+
+public class CalculateAverage {
+//    Calculate average
+//
+//    Write a function which calculates the average of the numbers in a
+//    given list.
+//
+//    Note: Empty arrays should return 0.
+
+
+
+    public static double find_average(int[] array){
+        if (array.length == 0) return 0;
+        double sum = 0;
+
+        for (int n: array){
+            sum += n;
+        }
+
+        return sum / array.length;
+    }
+}
