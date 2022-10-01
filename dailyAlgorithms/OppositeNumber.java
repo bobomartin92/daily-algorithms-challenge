e8206e65b77bf31e57639ffe2b836adef3c59716
Bodiseowei Martin Tonweriyai <bmtonweriyai@gmail.com>
2022-10-01 14:00:26 +0100

    opposite number solution

---
+++ dailyAlgorithms/OppositeNumber.java
@@ -0,0 +1,20 @@
+package dailyAlgorithms;
+
+public class OppositeNumber {
+
+//    Opposite number
+//    Very simple, given an integer or a floating-point number, find its opposite.
+//
+//    Examples:
+//
+//            1: -1
+//            14: -14
+//            -34: 34
+
+
+    public static int opposite(int number)
+    {
+        // your code here
+        return number * -1;
+    }
+}
