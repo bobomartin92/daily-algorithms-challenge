8a2c8d1981a20cf43e3ad6d4e44d7639f66e890a
Bodiseowei Martin Tonweriyai <bmtonweriyai@gmail.com>
2022-10-01 14:06:24 +0100

    make upper case solution

---
+++ dailyAlgorithms/MakeUpperCase.java
@@ -0,0 +1,11 @@
+package dailyAlgorithms;
+
+public class MakeUpperCase {
+//    MakeUpperCase
+//
+//    Write a function which converts the input string to uppercase.
+
+    public static String makeUpperCase(String str){
+        return str.toUpperCase();
+    }
+}
